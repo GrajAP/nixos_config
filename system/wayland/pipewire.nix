89edc77e59846d387662ac4f1f24b95448c05355
GrajAP <adampisarczyk2006@gmail.com>
2026-04-30 17:23:16 +0200

    2026-04-30 17:23

--- system/wayland/pipewire.nix
+++ system/wayland/pipewire.nix
@@ -9,8 +9,24 @@
         enable = true;
         support32Bit = true;
       };
-      wireplumber.enable = true;
-      pulse.enable = true;
+      wireplumber = {
+        enable = true;
+        extraConfig = {
+          "60-ee-default-source" = {
+            "monitor.alsa" = [
+              {
+                matches = [{node.name = "easyeffects_source";}];
+                actions = {
+                  update-props = {
+                    "session.priority" = 99;
+                  };
+                };
+              }
+            ];
+          };
+        };
+      };
+      pulse. enable = true;
       jack.enable = true;
     };
     pulseaudio.support32Bit = true;
