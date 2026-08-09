2e95b74f0e9ee4f1c65901a3f78db13a7dff8cd8
GrajAP <adampisarczyk2006@gmail.com>
2026-08-09 18:36:46 +0200

    chore(nixos): rebuild 2026-08-09 18:36

--- home/rice/hyprland/config.nix
+++ home/rice/hyprland/config.nix
@@ -34,7 +34,7 @@
       decoration = {
         rounding = 12;
         blur = {
-          enabled = true;
+          enabled = false;
           size = 3;
           passes = 3;
           ignore_opacity = false;
