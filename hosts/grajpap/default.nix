27d1c85ff5a5c8dad4fedb366c33bfe9b4c507c5
GrajAP <adampisarczyk2006@gmail.com>
2026-08-11 13:44:09 +0200

    chore(nixos): rebuild 2026-08-11 13:44

--- hosts/grajpap/default.nix
+++ hosts/grajpap/default.nix
@@ -46,7 +46,15 @@ in {
     acpi
     powertop
     libnotify
-    teamviewer
+    (teamviewer.overrideAttrs (old: {
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [makeWrapper];
+      postFixup =
+        (old.postFixup or "")
+        + ''
+          wrapProgram $out/bin/teamviewer --set QT_QPA_PLATFORM xcb
+          wrapProgram $out/share/teamviewer/tv_bin/script/teamviewer --set QT_QPA_PLATFORM xcb
+        '';
+    }))
   ];
 
   systemd.services.kanata-cs2-guard = {
