9c39446f3a7cfc431b6a9f950b2614e66bb61d4e
GrajAP <adampisarczyk2006@gmail.com>
2026-09-16 21:34:36 +0200

    chore(nixos): rebuild 2026-09-16 21:34

--- hosts/grajpap/default.nix
+++ hosts/grajpap/default.nix
@@ -91,6 +91,10 @@ in {
       keyboards = {
         internalKeyboard = {
           devices = [
+            "/dev/input/by-id/usb-Cooler_Master_Technology_Inc._MK730-event-kbd"
+            "/dev/input/by-id/usb-Cooler_Master_Technology_Inc._MK730-if02-event-kbd"
+            "/dev/input/by-path/pci-0000:09:00.3-usb-0:6.4:1.0-event-kbd"
+            "/dev/input/by-path/pci-0000:09:00.3-usb-0:6.4:1.2-event-kbd"
             "/dev/input/by-path/pci-0000:09:00.3-usb-0:6.2:1.0-event-kbd"
             "/dev/input/by-path/pci-0000:09:00.3-usb-0:6.2:1.2-event-kbd"
             "/dev/input/by-path/pci-0000:0e:00.3-usb-0:4.2:1.1-event-kbd"
