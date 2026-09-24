d93bf0e68466fd838fcf6682085492a9fa9a613e
GrajAP <adampisarczyk2006@gmail.com>
2026-09-24 12:51:57 +0200

    chore(nixos): rebuild 2026-09-24 12:51

--- home/features/development.nix
+++ home/features/development.nix
@@ -42,14 +42,14 @@ in {
 
   systemd.user = {
     services.t3code-update = {
-      Unit.Description = "Download the newest T3 Code nightly desktop build";
+      Unit.Description = "Download the newest stable T3 Code desktop build";
       Service = {
         Type = "oneshot";
         ExecStart = "${t3code.update}/bin/t3code-update";
       };
     };
     timers.t3code-update = {
-      Unit.Description = "Keep T3 Code on the latest nightly";
+      Unit.Description = "Keep T3 Code on the latest stable release";
       Timer = {
         OnStartupSec = "2min";
         OnUnitActiveSec = "30min";
