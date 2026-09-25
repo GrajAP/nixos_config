5507a389e2edd25017aafca1b61cf3b984b7f974
GrajAP <adampisarczyk2006@gmail.com>
2026-09-25 22:04:57 +0200

    chore(nixos): rebuild 2026-09-25 22:04

--- home/cli/zsh/tools.nix
+++ home/cli/zsh/tools.nix
@@ -3,9 +3,32 @@ _: {
     udiskie.enable = true;
     gpg-agent = {
       enable = true;
-      enableSshSupport = true;
+      enableSshSupport = false;
       enableZshIntegration = true;
     };
+  };
+  # SSH keys live in gnome-keyring (unlocked by PAM at login), not in
+  # gpg-agent, so point every local shell/service at the gcr socket.
+  sshAuthSock = {
+    enable = true;
+    initialization = {
+      bash = ''
+        if [ -S "$XDG_RUNTIME_DIR/gcr/ssh" ]; then
+          export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
+        fi
+      '';
+      fish = ''
+        if test -S "$XDG_RUNTIME_DIR/gcr/ssh"
+          set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
+        end
+      '';
+      nushell = ''
+        if ("$env.XDG_RUNTIME_DIR/gcr/ssh" | path exists) {
+          $env.SSH_AUTH_SOCK = $"($env.XDG_RUNTIME_DIR)/gcr/ssh"
+        }
+      '';
+    };
+    systemd.socketProviderUnit = "gcr-ssh-agent.socket";
   };
   programs = {
     gpg.enable = true;
